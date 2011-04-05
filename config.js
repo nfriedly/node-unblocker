643bd7922b01368f94f4cc277b1b4b3f3a542f24
Nathan Friedly <nathan@nfriedly.com>
2011-04-05 09:31:27 -0400

    local changes to config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // just "site.com" or "site.com:port", no "http://"
-exports.host = null;
+exports.host = "nodeunblocker.com";
 
 // string to specify this server's ip, or null to listen on any ip
 exports.ip = null;
 
-exports.port = 8080;
+exports.port = 8081;
 
  // if set, GA tracking code will be inserted into the bottom of every page
-exports.google_analytics_id = null;
+exports.google_analytics_id = 'UA-1735765-22';
