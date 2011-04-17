e6d3354a11cee92ac7409c4fb3d390fa46f7975c
Nathan Friedly <nathan@nfriedly.com>
2011-04-17 08:07:09 -0700

    Cleaned up config file

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // just "site.com" or "site.com:port", no "http://"
-exports.host = "nodeunblocker.com";
+exports.host = null;
 
 // string to specify this server's ip, or null to listen on any ip
 exports.ip = null;
 
-exports.port = 8081;
+exports.port = 8080;
 
  // if set, GA tracking code will be inserted into the bottom of every page
-exports.google_analytics_id = 'UA-1735765-22';
+exports.google_analytics_id = null;
