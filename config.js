8e99f6552c6919115f80bc5b740c0e3c67232337
unknown <nathan@nathan-i7.(none)>
2012-02-24 01:14:09 -0800

    tweaks to play nice with heroku

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.host = null;
 // string to specify this server's ip, or null to listen on any ip
 exports.ip = null;
 
-exports.port = 8080;
+exports.port = process.env.PORT || 8080;
 
  // if set, GA tracking code will be inserted into the bottom of every page
 exports.google_analytics_id = null;
