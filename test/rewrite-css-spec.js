e5ee452f9375955f4952de37796fdf1fb2187d08
Nathan Friedly <nathan@nfriedly.com>
2021-04-20 20:32:01 -0400

    WIP: splitting out parsing by type, adding a url rewriter to the request context

---
+++ test/rewrite-css-spec.js
@@ -0,0 +1,94 @@
+"use strict";
+
+//const { PassThrough } = require("stream");
+const { test } = require("tap");
+const _ = require("lodash");
+const concat = require("concat-stream");
+const RewriteCss = require("../lib/rewrite-css.js");
+const prefix = "/proxy/";
+const config = {
+  prefix,
+};
+const { fixCSS } = RewriteCss(config);
+
+const testUri = new URL("http://localhost:8081/");
+const cssTestLines = {
+  ".bg1 { background: url(http://example.com/img.jpg); }":
+    ".bg1 { background: url(/proxy/http://example.com/img.jpg); }",
+  ".bg2 { background: url(https://example.com/img.jpg); }":
+    ".bg2 { background: url(/proxy/https://example.com/img.jpg); }",
+  ".bg3 { background: url(//example.com/img.jpg); }":
+    ".bg3 { background: url(/proxy/http://example.com/img.jpg); }",
+  ".bg4 { background: url(/img.jpg); }":
+    ".bg4 { background: url(/proxy/http://localhost:8081/img.jpg); }",
+  ".bg5 { background: url(img.jpg); }": ".bg5 { background: url(img.jpg); }",
+  ".bg1 { background: url('http://example.com/img.jpg'); }":
+    ".bg1 { background: url('/proxy/http://example.com/img.jpg'); }",
+  ".bg2 { background: url('https://example.com/img.jpg'); }":
+    ".bg2 { background: url('/proxy/https://example.com/img.jpg'); }",
+  ".bg3 { background: url('//example.com/img.jpg'); }":
+    ".bg3 { background: url('/proxy/http://example.com/img.jpg'); }",
+  ".bg4 { background: url('/img.jpg'); }":
+    ".bg4 { background: url('/proxy/http://localhost:8081/img.jpg'); }",
+  ".bg5 { background: url('img.jpg'); }":
+    ".bg5 { background: url('img.jpg'); }",
+  '.bg1 { background: url("http://example.com/img.jpg"); }':
+    '.bg1 { background: url("/proxy/http://example.com/img.jpg"); }',
+  '.bg2 { background: url("https://example.com/img.jpg"); }':
+    '.bg2 { background: url("/proxy/https://example.com/img.jpg"); }',
+  '.bg3 { background: url("//example.com/img.jpg"); }':
+    '.bg3 { background: url("/proxy/http://example.com/img.jpg"); }',
+  '.bg4 { background: url("/img.jpg"); }':
+    '.bg4 { background: url("/proxy/http://localhost:8081/img.jpg"); }',
+  '.bg5 { background: url("img.jpg"); }':
+    '.bg5 { background: url("img.jpg"); }',
+  ".bg1 { background: url( http://example.com/img.jpg ); }":
+    ".bg1 { background: url( /proxy/http://example.com/img.jpg ); }",
+  ".bg2 { background: url( https://example.com/img.jpg ); }":
+    ".bg2 { background: url( /proxy/https://example.com/img.jpg ); }",
+  ".bg3 { background: url( //example.com/img.jpg ); }":
+    ".bg3 { background: url( /proxy/http://example.com/img.jpg ); }",
+  ".bg4 { background: url( /img.jpg ); }":
+    ".bg4 { background: url( /proxy/http://localhost:8081/img.jpg ); }",
+  ".bg5 { background: url( img.jpg ); }":
+    ".bg5 { background: url( img.jpg ); }",
+  ".bg1 { background: url( 'http://example.com/img.jpg' ); }":
+    ".bg1 { background: url( '/proxy/http://example.com/img.jpg' ); }",
+  ".bg2 { background: url( 'https://example.com/img.jpg' ); }":
+    ".bg2 { background: url( '/proxy/https://example.com/img.jpg' ); }",
+  ".bg3 { background: url( '//example.com/img.jpg' ); }":
+    ".bg3 { background: url( '/proxy/http://example.com/img.jpg' ); }",
+  ".bg4 { background: url( '/img.jpg' ); }":
+    ".bg4 { background: url( '/proxy/http://localhost:8081/img.jpg' ); }",
+  ".bg5 { background: url( 'img.jpg' ); }":
+    ".bg5 { background: url( 'img.jpg' ); }",
+  '.bg1 { background: url( "http://example.com/img.jpg" ); }':
+    '.bg1 { background: url( "/proxy/http://example.com/img.jpg" ); }',
+  '.bg2 { background: url( "https://example.com/img.jpg" ); }':
+    '.bg2 { background: url( "/proxy/https://example.com/img.jpg" ); }',
+  '.bg3 { background: url( "//example.com/img.jpg" ); }':
+    '.bg3 { background: url( "/proxy/http://example.com/img.jpg" ); }',
+  '.bg4 { background: url( "/img.jpg" ); }':
+    '.bg4 { background: url( "/proxy/http://localhost:8081/img.jpg" ); }',
+  '.bg5 { background: url( "img.jpg" ); }':
+    '.bg5 { background: url( "img.jpg" ); }',
+  '.bg4 { background: url(   "/img.jpg"   ); }':
+    '.bg4 { background: url(   "/proxy/http://localhost:8081/img.jpg"   ); }',
+  '.bg4 { background: url( "/img.jpg"  ); }':
+    '.bg4 { background: url( "/proxy/http://localhost:8081/img.jpg"  ); }',
+};
+
+test("should rewrite (or not rewrite) various CSS strings correctly", function (t) {
+  _.each(cssTestLines, function (expected, source) {
+    const actual = fixCSS(source, testUri, config);
+    t.equal(
+      actual,
+      expected,
+      "Should rewrite '" + source + "' to '" + expected + '"'
+    );
+  });
+  t.end();
+  concat;
+});
+
+// todo: test a stream, test in a style tag, test in an attribute
--- test/rewrite-html-spec.js
+++ test/rewrite-html-spec.js
@@ -4,8 +4,8 @@ const { PassThrough } = require("stream");
 const { test } = require("tap");
 const _ = require("lodash");
 const concat = require("concat-stream");
-
-const urlPrefix = require("../lib/url-prefixer.js")({
+const RewriteHtml = require("../lib/rewrite-html.js");
+const htmlRewriter = RewriteHtml({
   prefix: "/proxy/",
 });
 
@@ -117,7 +117,7 @@ const htmlTestLines = {
 };
 
 const testUri = new URL("http://localhost:8081/");
-const testPrefix = "/proxy/";
+//const testPrefix = "/proxy/";
 
 _.each(htmlTestLines, function (expected, source) {
   test(`should rewrite ${source} to ${expected}`, function (t) {
@@ -129,7 +129,7 @@ _.each(htmlTestLines, function (expected, source) {
       stream: sourceStream,
       contentType: "text/html",
     };
-    urlPrefix(data);
+    htmlRewriter(data);
     data.stream.setEncoding("utf8");
     data.stream.pipe(
       concat((actual) => {
@@ -140,155 +140,3 @@ _.each(htmlTestLines, function (expected, source) {
     sourceStream.end(source);
   });
 });
-
-const cssTestLines = {
-  ".bg1 { background: url(http://example.com/img.jpg); }":
-    ".bg1 { background: url(/proxy/http://example.com/img.jpg); }",
-  ".bg2 { background: url(https://example.com/img.jpg); }":
-    ".bg2 { background: url(/proxy/https://example.com/img.jpg); }",
-  ".bg3 { background: url(//example.com/img.jpg); }":
-    ".bg3 { background: url(/proxy/http://example.com/img.jpg); }",
-  ".bg4 { background: url(/img.jpg); }":
-    ".bg4 { background: url(/proxy/http://localhost:8081/img.jpg); }",
-  ".bg5 { background: url(img.jpg); }": ".bg5 { background: url(img.jpg); }",
-  ".bg1 { background: url('http://example.com/img.jpg'); }":
-    ".bg1 { background: url('/proxy/http://example.com/img.jpg'); }",
-  ".bg2 { background: url('https://example.com/img.jpg'); }":
-    ".bg2 { background: url('/proxy/https://example.com/img.jpg'); }",
-  ".bg3 { background: url('//example.com/img.jpg'); }":
-    ".bg3 { background: url('/proxy/http://example.com/img.jpg'); }",
-  ".bg4 { background: url('/img.jpg'); }":
-    ".bg4 { background: url('/proxy/http://localhost:8081/img.jpg'); }",
-  ".bg5 { background: url('img.jpg'); }":
-    ".bg5 { background: url('img.jpg'); }",
-  '.bg1 { background: url("http://example.com/img.jpg"); }':
-    '.bg1 { background: url("/proxy/http://example.com/img.jpg"); }',
-  '.bg2 { background: url("https://example.com/img.jpg"); }':
-    '.bg2 { background: url("/proxy/https://example.com/img.jpg"); }',
-  '.bg3 { background: url("//example.com/img.jpg"); }':
-    '.bg3 { background: url("/proxy/http://example.com/img.jpg"); }',
-  '.bg4 { background: url("/img.jpg"); }':
-    '.bg4 { background: url("/proxy/http://localhost:8081/img.jpg"); }',
-  '.bg5 { background: url("img.jpg"); }':
-    '.bg5 { background: url("img.jpg"); }',
-  ".bg1 { background: url( http://example.com/img.jpg ); }":
-    ".bg1 { background: url( /proxy/http://example.com/img.jpg ); }",
-  ".bg2 { background: url( https://example.com/img.jpg ); }":
-    ".bg2 { background: url( /proxy/https://example.com/img.jpg ); }",
-  ".bg3 { background: url( //example.com/img.jpg ); }":
-    ".bg3 { background: url( /proxy/http://example.com/img.jpg ); }",
-  ".bg4 { background: url( /img.jpg ); }":
-    ".bg4 { background: url( /proxy/http://localhost:8081/img.jpg ); }",
-  ".bg5 { background: url( img.jpg ); }":
-    ".bg5 { background: url( img.jpg ); }",
-  ".bg1 { background: url( 'http://example.com/img.jpg' ); }":
-    ".bg1 { background: url( '/proxy/http://example.com/img.jpg' ); }",
-  ".bg2 { background: url( 'https://example.com/img.jpg' ); }":
-    ".bg2 { background: url( '/proxy/https://example.com/img.jpg' ); }",
-  ".bg3 { background: url( '//example.com/img.jpg' ); }":
-    ".bg3 { background: url( '/proxy/http://example.com/img.jpg' ); }",
-  ".bg4 { background: url( '/img.jpg' ); }":
-    ".bg4 { background: url( '/proxy/http://localhost:8081/img.jpg' ); }",
-  ".bg5 { background: url( 'img.jpg' ); }":
-    ".bg5 { background: url( 'img.jpg' ); }",
-  '.bg1 { background: url( "http://example.com/img.jpg" ); }':
-    '.bg1 { background: url( "/proxy/http://example.com/img.jpg" ); }',
-  '.bg2 { background: url( "https://example.com/img.jpg" ); }':
-    '.bg2 { background: url( "/proxy/https://example.com/img.jpg" ); }',
-  '.bg3 { background: url( "//example.com/img.jpg" ); }':
-    '.bg3 { background: url( "/proxy/http://example.com/img.jpg" ); }',
-  '.bg4 { background: url( "/img.jpg" ); }':
-    '.bg4 { background: url( "/proxy/http://localhost:8081/img.jpg" ); }',
-  '.bg5 { background: url( "img.jpg" ); }':
-    '.bg5 { background: url( "img.jpg" ); }',
-  '.bg4 { background: url(   "/img.jpg"   ); }':
-    '.bg4 { background: url(   "/proxy/http://localhost:8081/img.jpg"   ); }',
-  '.bg4 { background: url( "/img.jpg"  ); }':
-    '.bg4 { background: url( "/proxy/http://localhost:8081/img.jpg"  ); }',
-};
-
-test("should rewrite (or not rewrite) various CSS strings correctly", function (t) {
-  _.each(cssTestLines, function (expected, source) {
-    const actual = urlPrefix.rewriteUrls(source, testUri, testPrefix);
-    t.equal(
-      actual,
-      expected,
-      "Should rewrite '" + source + "' to '" + expected + '"'
-    );
-  });
-  t.end();
-  concat;
-});
-
-const fullSource = Object.keys(htmlTestLines)
-  .concat("<style>", Object.keys(cssTestLines), "</style>")
-  .join("\n");
-const fullExpected = Object.values(htmlTestLines)
-  .concat("<style>", Object.values(cssTestLines), "</style>")
-  .join("\n");
-
-test(`should rewrite the whole thing`, function (t) {
-  t.plan(1);
-  const sourceStream = new PassThrough();
-  sourceStream.setEncoding("utf8");
-  const data = {
-    url: testUri.href,
-    stream: sourceStream,
-    contentType: "text/html",
-  };
-  urlPrefix(data);
-  data.stream.setEncoding("utf8");
-  data.stream.pipe(
-    concat((actual) => {
-      t.equal(actual, fullExpected);
-      t.end();
-    })
-  );
-  sourceStream.end(fullSource);
-});
-
-test("should correctly handle packets split at different locations", function (t) {
-  function createSubTest(start, end) {
-    // this causes the following warning:
-    // (node) warning: Recursive process.nextTick detected. This will break in the next version of node. Please use setImmediate for recursive deferral.
-    //t.test("Should handle breaks between '" + start.substr(-20) + "' and '" + end.substr(0,20) + "' correctly", function(t) {
-    const sourceStream = new PassThrough();
-    sourceStream.setEncoding("utf8");
-    const data = {
-      url: testUri.href,
-      stream: sourceStream,
-      contentType: "text/html",
-    };
-    urlPrefix(data);
-    data.stream.setEncoding("utf8");
-    data.stream.pipe(
-      concat((actual) => {
-        t.equal(
-          actual,
-          fullExpected,
-          "Should handle chunk breaks between '" +
-            start.substr(-20) +
-            "' and '" +
-            end.substr(0, 20) +
-            "' correctly"
-        );
-        if (actual != fullExpected) throw "stopping early";
-      })
-    );
-    sourceStream.write(start);
-    sourceStream.end(end);
-    //});
-  }
-
-  t.plan(fullSource.length);
-  for (
-    let splitLocation = 0, l = fullSource.length;
-    splitLocation < l;
-    splitLocation++
-  ) {
-    const start = fullSource.substr(0, splitLocation);
-    const end = fullSource.substr(splitLocation);
-    createSubTest(start, end);
-  }
-});
-// todo: add tests for javascript (?)
